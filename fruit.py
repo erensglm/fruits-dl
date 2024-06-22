4ad3a0bc897ae76496ba9ed450b979d3d90a76fa
erensglm <101216457+erensglm@users.noreply.github.com>
2024-06-22 13:01:44 +0300

    .

--- fruit.py
+++ fruit.py
@@ -5,8 +5,8 @@ from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 from  glob import  glob
 import matplotlib.pyplot as plt
 
-train_path= "C:\\archive\\fruits-360_dataset\\fruits-360\\Training\\"
-test_path= "C:\\archive\\fruits-360_dataset\\fruits-360\\Test\\"
+train_path= "C:\\Users\\erens\\OneDrive\\Desktop\\EREN\\DERS\\DerinOgrenmeyeGiris\\fruit-360dataset\\fruits-360_dataset\\fruits-360\\Training\\"
+test_path= "C:\\Users\\erens\\OneDrive\\Desktop\\EREN\\DERS\\DerinOgrenmeyeGiris\\fruit-360dataset\\fruits-360_dataset\\fruits-360\\Test\\"
 
 img=load_img(train_path+"Apple Braeburn\\0_100.jpg")
 #plt.imshow(img)
